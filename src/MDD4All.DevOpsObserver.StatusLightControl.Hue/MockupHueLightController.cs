64b2aacc689dbe5f3b285554ceb83041b006c0d8
oalt <oa@lacheweg21.de>
2026-06-18 09:48:10 +0200

    Adding mockup class, usable for dependency injection when no hardware is connected

---
+++ src/MDD4All.DevOpsObserver.StatusLightControl.Hue/MockupHueLightController.cs
@@ -0,0 +1,33 @@
+﻿using MDD4All.DevOpsObserver.DataModels;
+using MDD4All.DevOpsObserver.StatusLightControl.Contracts;
+using System.Threading.Tasks;
+
+namespace MDD4All.DevOpsObserver.StatusLightControl.Hue
+{
+    public class MockupHueLightController : IStatusLightController
+    {
+        public DevOpsStatus CurrentStatus { get; private set; } = DevOpsStatus.Unknown;
+
+        public bool IsOn { get; private set; }
+
+        public Task ActivateAlertAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SetStatusLightAsync(DevOpsStatus devOpsStatus)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task TurnLightOffAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task TurnLightOnAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
